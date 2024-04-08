015919e826a0d2c66560e614027bfce823fb3805
aya.okamoto <aya.okamoto.u004@dirbato.co.jp>
2024-04-08 12:29:17 +0900

    Lesson2　のサンプルです

---
+++ Sample/src/sample/Forsample.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Forsample {
+
+    public static void main(String[] args) {
+            for (var i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+
+    }
+
+}
